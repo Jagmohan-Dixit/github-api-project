fc836fb03a66ac12ed83ac8fa818b8b23da0dfed
sarthak503 <sarthak.kumar503@gmail.com>
2022-02-02 11:39:38 +0530

    added js functionality

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-var form = document.getElementById("form")
+var form = document.getElementById("form");
+document.getElementById("result").style.marginBottom = 0;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  var search = document.getElementById("search").value;
+  var searchName = search.split(" ").join("");
 
-var search_name = document.getElementById("search")
+  document.getElementById("result").innerHTML = "";
 
-fetch("https://api.github.com/users/"+search_name)
-.then((res) => res.json())
-.then(data => {
-    console.log(data)
-})
+  fetch("https://api.github.com/users/" + searchName)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("result").innerHTML = `
+        <a target="_blank" href="https://github.com/${searchName}"><img src="${data.avatar_url}"></img></a>
+      `;
+    });
+  document.getElementById("result").style.marginBottom = "-15%";
+});
